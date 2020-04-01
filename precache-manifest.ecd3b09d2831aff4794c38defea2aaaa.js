048e7794e61a229c42e15e782e90cbb4a7c9cda3
Jérémy Simonklein Angély <jeremy.simonklein@gmail.com>
2020-04-01 19:02:38 +0200

    Updates

---
+++ precache-manifest.ecd3b09d2831aff4794c38defea2aaaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ed34f39785711c5b406624058f75549",
+    "url": "/tdforever/index.html"
+  },
+  {
+    "revision": "ad41410202e2e2455392",
+    "url": "/tdforever/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1a275f5e586f86fdf5e8",
+    "url": "/tdforever/static/js/2.6e455239.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tdforever/static/js/2.6e455239.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad41410202e2e2455392",
+    "url": "/tdforever/static/js/main.fb717832.chunk.js"
+  },
+  {
+    "revision": "e22a661d6647f40b1125",
+    "url": "/tdforever/static/js/runtime-main.3e95ebff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tdforever/static/media/logo.5d5d9eef.svg"
+  }
+]);
